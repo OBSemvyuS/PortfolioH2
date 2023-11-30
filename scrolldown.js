ccbb40d17f3d29cb693c5a90b12b0bdcfb2450d3
OBSemvyuS <l.ninine@hetic.eu>
2023-11-30 20:29:21 +0100

    Quick fix of the scrollbar script

--- scrolldown.js
+++ scrolldown.js
@@ -33,25 +33,25 @@ function getDocumentTop(element) {
   return rect.top + scrollTop;
 }
 
-document.querySelectorAll("nav h5")[0].addEventListener("click", function () {
+document.querySelectorAll("nav div")[0].addEventListener("click", function () {
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
 
-document.querySelectorAll("nav h5")[1].addEventListener("click", function () {
+document.querySelectorAll("nav div")[1].addEventListener("click", function () {
   var elementPosition = getDocumentTop(
     document.querySelector("main > section")
   );
   window.scrollTo({ top: elementPosition - offset, behavior: "smooth" });
 });
 
-document.querySelectorAll("nav h5")[2].addEventListener("click", function () {
+document.querySelectorAll("nav div")[2].addEventListener("click", function () {
   var elementPosition = getDocumentTop(
     document.querySelectorAll("main > section")[1]
   );
   window.scrollTo({ top: elementPosition - offset, behavior: "smooth" });
 });
 
-document.querySelectorAll("nav h5")[3].addEventListener("click", function () {
+document.querySelectorAll("nav div")[3].addEventListener("click", function () {
   var elementPosition = getDocumentTop(document.querySelector("footer"));
   window.scrollTo({ top: elementPosition - offset, behavior: "smooth" });
 });
