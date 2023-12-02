4d1f2bcd06323af5452ad7bfa82c472ead76c5f4
OBSemvyuS <l.ninine@hetic.eu>
2023-12-02 17:57:34 +0100

    I have a working slider (finally)

---
+++ js/slider.js
@@ -0,0 +1,90 @@
+let images = [...document.querySelectorAll(".project__card .img")];
+let slider = document.querySelector(".slider");
+let sliderWidth;
+let imageWidth;
+let isDragging = false;
+let startX;
+let offsetX = 0;
+let current = 0;
+let target = 0;
+let ease = 0.04;
+
+window.addEventListener("resize", init);
+
+images.forEach((img, idx) => {
+  img.style.backgroundImage = `url(./images/${idx + 1}.jpg)`;
+});
+
+function lerp(start, end, t) {
+  return start * (1 - t) + end * t;
+}
+
+function setTransform(el, transform) {
+  el.style.transform = transform;
+}
+
+function init() {
+  sliderWidth = slider.getBoundingClientRect().width;
+  imageWidth = sliderWidth / images.length;
+  slider.style.width = `${imageWidth * images.length}px`; 
+  document.body.style.height = `${
+    sliderWidth - (window.innerWidth - window.innerHeight)
+  }px`;
+
+  slider.addEventListener("mousedown", (e) => {
+    isDragging = true;
+    startX = e.clientX;
+    offsetX = current;
+    e.preventDefault(); 
+  });
+
+  slider.addEventListener("mouseup", () => {
+    isDragging = false;
+  });
+
+  document.addEventListener("mousemove", (e) => {
+    if (isDragging) {
+      const mouseX = e.clientX;
+      const dragDistance = mouseX - startX;
+      target = offsetX - dragDistance;
+      e.preventDefault(); 
+    }
+  });
+
+  window.addEventListener("resize", () => {
+    sliderWidth = slider.getBoundingClientRect().width;
+    imageWidth = sliderWidth / images.length;
+    slider.style.width = `${imageWidth * images.length}px`;
+    document.body.style.height = `${
+      sliderWidth - (window.innerWidth - window.innerHeight)
+    }px`;
+  });
+}
+
+function animate() {
+  let maxScroll =
+    sliderWidth - window.innerWidth + (window.innerWidth * 4.16) / 100;
+  if (target > maxScroll) target = maxScroll;
+
+  current = lerp(current, target, ease);
+  setTransform(slider, `translateX(-${current}px)`);
+  animateImages();
+  requestAnimationFrame(animate);
+}
+
+function animateImages() {
+  let ratio = current / imageWidth;
+  let intersectionRatioValue;
+
+  images.forEach((image, idx) => {
+    intersectionRatioValue = ratio - idx * 0.001;
+    setTransform(image, `translateX(${intersectionRatioValue * 70}px)`);
+  });
+}
+
+document.addEventListener("mouseup", () => {
+  isDragging = false;
+});
+
+init();
+animate();
