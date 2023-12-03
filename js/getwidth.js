77ca1f9c12a806935793ae9b6cfa75c7abd51f1d
OBSemvyuS <l.ninine@hetic.eu>
2023-12-03 19:35:43 +0100

    Cursor modifications

--- js/getwidth.js
+++
@@ -1,5 +0,0 @@
-// let referenceElement = document.querySelector('.project__card');
-// let targetElement = document.querySelector('.project__info__container');
-
-// let referenceElementWidth = referenceElement.getBoundingClientRect().width;
-// targetElement.style.width = `${referenceElementWidth}px`;
